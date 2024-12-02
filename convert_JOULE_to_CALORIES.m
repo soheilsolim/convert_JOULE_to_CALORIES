6d94dfabb5ac9eb81c129fa4603f938a590aad56
soheil <140661028+soheilsolim@users.noreply.github.com>
2024-12-02 23:09:38 +0330

    Add files via upload

---
+++ convert_JOULE_to_CALORIES.m
@@ -0,0 +1,7 @@
+%soheil mansouri (electrical engineering)
+%Script file to convert joule to calories
+%
+JOULE=input('number in JOULE:');  %give JOULE
+CALORIES=(JOULE*0.0002388459);
+disp(['number in CALORIES=',num2str(CALORIES)])  %get CALORIES
+%stop
